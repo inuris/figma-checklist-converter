e1d1c4e9571aa95fd93d3899b55c65b41e4a97d4
Khoa Lam <lakhoa1989@gmail.com>
2026-03-29 13:17:18 +0700

    [docs/icon.svg] Add new SVG icon

--- scripts/build-web.mjs
+++ scripts/build-web.mjs
@@ -8,6 +8,8 @@ const docs = join(root, 'docs');
 
 mkdirSync(docs, { recursive: true });
 copyFileSync(join(root, 'web', 'index.html'), join(docs, 'index.html'));
+copyFileSync(join(root, 'assets', 'icon.svg'), join(docs, 'icon.svg'));
+copyFileSync(join(root, 'assets', 'icon.png'), join(docs, 'icon.png'));
 writeFileSync(join(docs, '.nojekyll'), '');
 
 await esbuild.build({
